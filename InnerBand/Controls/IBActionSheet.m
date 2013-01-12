5bdb59ca27ceaa841dc5efdfaf0f7bef2940beda
John Blanco <john.blanco@raptureinvenice.com>
2013-01-11 18:08:51 -0700

    Fixed issue with IBActionSheet where "other buttons" wouldn't display more than 1.

--- InnerBand/Controls/IBActionSheet.m
+++ InnerBand/Controls/IBActionSheet.m
@@ -11,7 +11,19 @@
 @implementation IBActionSheet
 
 - (id)initWithTitle:(NSString *)title cancelBlock:(void (^)(void))cancelBlock actionBlock:(void (^)(NSInteger))actionBlock cancelButtonTitle:(NSString *)cancelButtonTitle destructiveButtonTitle:(NSString *)destructiveButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... {
-    if ((self = [super initWithTitle:title delegate:self cancelButtonTitle:cancelButtonTitle destructiveButtonTitle:destructiveButtonTitle otherButtonTitles:otherButtonTitles, nil])) {
+    if ((self = [super initWithTitle:title delegate:self cancelButtonTitle:cancelButtonTitle destructiveButtonTitle:destructiveButtonTitle otherButtonTitles:nil])) {
+        [self addButtonWithTitle:otherButtonTitles];
+
+        va_list args;
+        va_start(args, otherButtonTitles);
+
+        NSString * title = nil;
+        while ((title = va_arg(args, NSString *))) {
+            [self addButtonWithTitle:title];
+        }
+
+        va_end(args);
+        
         actionBlock_ = [actionBlock copy];
         cancelBlock_ = [cancelBlock copy];
     }
