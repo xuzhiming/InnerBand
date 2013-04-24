6215df348a49dc0e627299b8b63ba1cb15ba11bf
John Blanco <john.blanco@raptureinvenice.com>
2013-04-24 13:17:05 -0600

    IBActionSheet was not presenting cancel and destructive buttons properly.
    * Destructive always on top.
    * Cancel always on bottom.
    * If you don't define a callback for cancel or other buttons, won't crash.

--- InnerBand/Controls/IBActionSheet.m
+++ InnerBand/Controls/IBActionSheet.m
@@ -11,7 +11,12 @@
 @implementation IBActionSheet
 
 - (id)initWithTitle:(NSString *)title cancelBlock:(void (^)(void))cancelBlock actionBlock:(void (^)(NSInteger))actionBlock cancelButtonTitle:(NSString *)cancelButtonTitle destructiveButtonTitle:(NSString *)destructiveButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... {
-    if ((self = [super initWithTitle:title delegate:self cancelButtonTitle:cancelButtonTitle destructiveButtonTitle:destructiveButtonTitle otherButtonTitles:nil])) {
+    if ((self = [super initWithTitle:title delegate:self cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil])) {
+        if (destructiveButtonTitle) {
+            [self addButtonWithTitle:destructiveButtonTitle];
+            self.destructiveButtonIndex = 0;
+        }
+
         [self addButtonWithTitle:otherButtonTitles];
 
         va_list args;
@@ -22,12 +27,17 @@
             [self addButtonWithTitle:title];
         }
 
+        if (cancelButtonTitle) {
+            [self addButtonWithTitle:cancelButtonTitle];
+            self.cancelButtonIndex = (self.numberOfButtons - 1);
+        }
+
         va_end(args);
-        
+
         actionBlock_ = [actionBlock copy];
         cancelBlock_ = [cancelBlock copy];
     }
-
+    
     return self;
 }
 
@@ -35,9 +45,13 @@
 
 - (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
     if (buttonIndex == self.cancelButtonIndex) {
-        cancelBlock_();
+        if (cancelBlock_) {
+            cancelBlock_();
+        }
     } else {
-        actionBlock_(buttonIndex);
+        if (actionBlock_) {
+            actionBlock_(buttonIndex);
+        }
     }
 }
 
